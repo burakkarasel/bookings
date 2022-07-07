53ae08eab13e900ef0ce95e3d976800fe4bdefe1
kullop <burakcankarasel@gmail.com>
2022-07-07 13:44:34 +0300

    Addition of templates for the website, addition of static css file, creating handler and routes for new templates and adding CSRF token for my requests

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"github.com/burakkarasel/bookings/pkg/config"
 	"github.com/burakkarasel/bookings/pkg/models"
 	"github.com/burakkarasel/bookings/pkg/utils"
@@ -26,7 +27,7 @@ func NewHandlers(r *Repository) {
 func (repo *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	remoteIP := r.RemoteAddr
 	repo.App.Session.Put(r.Context(), "remote_ip", remoteIP)
-	utils.RenderTemplate(w, "home.page.gohtml", &models.TemplateData{})
+	utils.RenderTemplate(w, r, "home.page.gohtml", &models.TemplateData{})
 }
 
 func (repo *Repository) About(w http.ResponseWriter, r *http.Request) {
@@ -36,7 +37,39 @@ func (repo *Repository) About(w http.ResponseWriter, r *http.Request) {
 	remoteIP := repo.App.Session.GetString(r.Context(), "remote_ip")
 	stringMap["remote_ip"] = remoteIP
 
-	utils.RenderTemplate(w, "about.page.gohtml", &models.TemplateData{
+	utils.RenderTemplate(w, r, "about.page.gohtml", &models.TemplateData{
 		StringMap: stringMap,
 	})
+}
+
+// Generals renders the room's page
+func (repo *Repository) Generals(w http.ResponseWriter, r *http.Request) {
+	utils.RenderTemplate(w, r, "generals.page.gohtml", &models.TemplateData{})
+}
+
+// Majors renders the room's page
+func (repo *Repository) Majors(w http.ResponseWriter, r *http.Request) {
+	utils.RenderTemplate(w, r, "majors.page.gohtml", &models.TemplateData{})
+}
+
+// Availability renders the search page for availability
+func (repo *Repository) Availability(w http.ResponseWriter, r *http.Request) {
+	utils.RenderTemplate(w, r, "search-availability.page.gohtml", &models.TemplateData{})
+}
+
+// Contact renders the contact page
+func (repo *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	utils.RenderTemplate(w, r, "contact.page.gohtml", &models.TemplateData{})
+}
+
+// MakeReservation renders our reservation page
+func (repo *Repository) MakeReservation(w http.ResponseWriter, r *http.Request) {
+	utils.RenderTemplate(w, r, "make-reservation.page.gohtml", &models.TemplateData{})
+}
+
+// PostAvailability sends our request
+func (repo *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
+	start := r.Form.Get("start")
+	end := r.Form.Get("end")
+	w.Write([]byte(fmt.Sprintf("You made your reservation from %s to %s!\n", start, end)))
 }
